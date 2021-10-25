c26595e3bb66c498fcc6fd796c08e3e7b835e5e2
abdurrakib-brur <93143775+abdurrakib-brur@users.noreply.github.com>
2021-10-25 21:51:43 +0600

    Add files via upload

---
+++ url-by-time.js
@@ -0,0 +1,29 @@
+<script>
+  function urlByTime(set_h, set_m, set_url){
+    var d = new Date("July 21, 1983 01:15:00");
+    var hour = d.getUTCHours();
+      function addZero(i) {
+        if (i < 10) {
+            i = "0" + i
+          }
+      return i;
+    }
+
+      var d = new Date();
+      var h = addZero(d.getUTCHours());
+      var m = addZero(d.getUTCMinutes());
+      var s = addZero(d.getUTCSeconds());
+      //var time = h + ":" + m + ":" + s;
+      
+      
+      //var set_h, set_m, set_url;
+      
+    
+      if(h>=set_h && m>=set_m){
+        window.open(set_url, '_blank');
+      }else{
+      
+        alert("It's not time yet!");
+      }
+  }
+</script>
